344bee59dcdd902bb5b864768deaa5c4cecfcd70
Erick Henrique <erick.henrique@aluno.ufabc.edu.br>
2021-11-22 15:27:50 -0300

    incluido log

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const rpa = require('../rpa')
 
 router.get('/:name', async (req, res) => {
+    console.log('recebi requisição ' + req.params.name)
     rpa.pesquisa(res, req.params.name);
 })
 
